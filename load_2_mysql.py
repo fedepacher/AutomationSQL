e27a8c7c79a30bff7f531c8267472c8c2f95a883
fedepacher <fede_pacher@hotmail.com>
2023-04-21 18:26:24 -0300

    add: add script to automate sql load files

---
+++ load_2_mysql.py
@@ -0,0 +1,214 @@
+"""Module providing file insertion to mysql database."""
+import os
+import sys
+import subprocess
+import platform
+import shutil
+import re
+import argparse
+import json
+import pandas as pd
+
+from mysql_lib import MySQLClass
+
+
+def get_path(os_var=''):
+    """Get path to read and store csv files.
+
+    Args:
+        os_var (str): Operating system.
+
+    Returns:
+        (str): Folder to load files.
+        (str): Folder to store csv files.
+    """
+    csv_path = ''
+    load_path = ''
+
+    return load_path, csv_path
+
+
+def create_csv_store_path(csv_path=''):
+    """Create folder to store csv files if not exist.
+
+    Args:
+        csv_path (str): Path where CSV files will be stored.
+    """
+    pass
+
+
+def convert_file_to_csv(load_path='', csv_path=''):
+    """Convert xls file to csv and store csv files to new folder.
+
+    Args:
+        load_path (str): Path that contains files to be converted.
+        csv_path (str): Path where CSV files will be stored.
+    """
+    pass
+
+
+def get_column_from_csv(csv_table_files=None, separator_list=None, column_list=None, csv_path=''):
+    """Get column names list from csv files.
+
+    Args:
+        csv_table_files (list): File list that contains csv files.
+        separator_list (list): List where separator will be stored.
+        column_list (list): List where csv columns will be stored.
+        csv_path (str): Path where CSV files are stored.
+    """
+    pass
+
+
+def check_unknow_char(column_list=None):
+    """Check for unknow characters when files are not UTF-8.
+
+     Args:
+        column_list (list): Column list to be returned.
+    """
+    pass
+
+
+def get_query_table(csv_table_files=None, column_list=None, query_list=None):
+    """Get queries to create tables.
+
+    Args:
+        csv_table_files (list): File list that contains csv files.
+        column_list (list): List where csv columns are stored.
+        query_list (list): List of query to create MySQL table.
+    """
+    pass
+
+
+def get_passwords():
+    """Get Workbench and sudo passwords from internal file.
+
+    Returns:
+        (str): Database Workbench password.
+        (str): Linux admin password.
+    """
+    pass_db = ''
+    pass_sudo = ''
+
+    return pass_db, pass_sudo
+
+
+def database_function(connection=None, db_name=''):
+    """Create database if not exist and set database internal configurations.
+
+    Args:
+        connection (mysql_lib.MySQLClass): Connection object to mysql.
+        db_name (str): Database name.
+    """
+    pass
+
+
+def database_create_tables(connection=None, query_list=None):
+    """Create database tables.
+
+    Args:
+        connection (mysql_lib.MySQLClass): Connection object to mysql.
+        query_list (list): Query list to create tables.
+    """
+    pass
+
+
+def copy_file_to_mysql_folder(csv_table_files=None, os_var='', db_name='', csv_path='',
+                              pass_sudo=''):
+    """Copy files to mysql folder to be read by Workbench.
+
+    Args:
+        csv_table_files (list): CSV files list.
+        os_var (str): Operating system.
+        db_name (str): Database name.
+        csv_path (str): Path where CSV files are stored.
+        pass_sudo (str): Linux admin password.
+    """
+    pass
+
+
+def fill_database_tables(connection=None, csv_table_files=None, separator_list=None,
+                         column_list=None):
+    """Load database tables with CSV files
+
+    Args:
+        connection (mysql_lib.MySQLClass): Connection object to mysql.
+        csv_table_files (list): CSV files list.
+        separator_list (list): List with CVS columns separator.
+        column_list (list): Table column list.
+    """
+    pass
+
+
+def run():
+    """Execute the code to convert csv files into MySQL database"""
+    column_list = []
+    separator_list = []
+    query_list = []
+
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument('-t', '--host_name', metavar='<host_name>', type=str, required=False,
+                            default='localhost', help='Workbench hostname', dest='host_name')
+    arg_parser.add_argument('-u', '--user_name', metavar='<user_name>', type=str, required=False,
+                            default='root', help='Workbench username', dest='user_name')
+    arg_parser.add_argument('-d', '--database', metavar='<database>', type=str, required=False,
+                            default='test_db', help='Database name', dest='db_name')
+
+    args = arg_parser.parse_args()
+
+    db_name = args.db_name
+    os_var = platform.system()
+
+    # Get path for storing and reading csv file
+    load_path, csv_path = get_path(os_var=os_var)
+
+    # Create csv_path directory if not exist
+    create_csv_store_path(csv_path=csv_path)
+
+    # Convert xls file to csv and copy files to new folder
+    convert_file_to_csv(load_path=load_path, csv_path=csv_path)
+
+    file_list = os.listdir(csv_path)
+
+    # Get CSV files
+    csv_table_files = [file for file in file_list if 'csv' in file]
+
+    # Get column and return list of column and separators
+    get_column_from_csv(csv_table_files=csv_table_files, separator_list=separator_list,
+                        column_list=column_list, csv_path=csv_path)
+
+    # Check for unknow characters when files are not UTF-8 and return value in clolumn_list
+    check_unknow_char(column_list=column_list)
+
+    # Get queries to create tables, return value in query_list
+    get_query_table(query_list=query_list, csv_table_files=csv_table_files, column_list=column_list)
+
+    # Get Workbench and super user Linux password passwords to copy file as admin
+    pass_db, pass_sudo = get_passwords()
+
+    # Connect to mySQL data base
+    connection = MySQLClass(password=pass_db)
+
+    # Create database
+    database_function(connection=connection, db_name=db_name)
+
+    # Create tables
+    database_create_tables(connection=connection, query_list=query_list)
+
+    # Copy files to mysql folder
+    copy_file_to_mysql_folder(csv_table_files=csv_table_files, os_var=os_var, db_name=db_name,
+                              csv_path=csv_path, pass_sudo=pass_sudo)
+
+    # Fill tables
+    fill_database_tables(connection=connection, csv_table_files=csv_table_files,
+                         separator_list=separator_list, column_list=column_list)
+
+
+# Driver Code
+if __name__ == '__main__' :
+
+    # Start script
+    print('Create Database\n')
+
+    # calling run function
+    run()
